ef0bddacd5d301b7c583e05628cc2e20de8c98cf
StepanGulyaev <91908161+StepanGulyaev@users.noreply.github.com>
2021-12-23 23:39:47 +0300

    Update LoginClass.cs

--- ClassLib/ClassLib/LoginClass.cs
+++ ClassLib/ClassLib/LoginClass.cs
@@ -9,10 +9,10 @@ using System.IO;
 using Newtonsoft.Json;
 
 namespace ClassLib
-{
+    {
     [Serializable]
     public class LoginClass
-    {
+        {
         public string login { get; set; }
         public string password { get; set; }
         private string token;
@@ -20,46 +20,43 @@ namespace ClassLib
         private DateTime elapsedRoundTime;
         private bool round;
         public void UpdateElapsedRoundTime(DateTime _eRT, bool _round)
-        {
+            {
             elapsedRoundTime = _eRT;
             round = _round;
-        }
+            }
 
         public string getElapsedTime()
-        {
+            {
             elapsedRoundTime.ToString("mm:ss");
             return elapsedRoundTime.ToString("mm:ss"); ;
-        }
+            }
         public LoginClass(string _login, string _password)
-        {
+            {
             this.login = _login;
             this.password = _password;
-        }
+            }
         public LoginClass()
-        {
-        }
+            {
+            }
         public string GenToken()
-        {
-            string salt = "СОЛЬ ЧТОБЫ НИКТО НЕ ДОГАДАЛСЯ;)";
+            {
             byte[] time = BitConverter.GetBytes(DateTime.UtcNow.ToBinary());
             byte[] key = Guid.NewGuid().ToByteArray();
             token = Convert.ToBase64String(time.Concat(key).ToArray());
             return token;
-        }
+            }
         public string GetToken()
-        {
+            {
             return this.token;
-        }
+            }
         public DateTime setTimeStamp()
-        {
+            {
             timeStamp = DateTime.Now;
             return timeStamp;
-        }
+            }
         public override string ToString()
-        {
+            {
             return String.Format($"login:{login} password:{password} token:{token} timeStamp: {timeStamp}");
+            }
         }
-
-        
     }
-}
