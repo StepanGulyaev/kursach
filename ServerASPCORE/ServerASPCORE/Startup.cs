f463d5046a67f7df09ec4fd299d71df383bd2327
StepanGulyaev <91908161+StepanGulyaev@users.noreply.github.com>
2021-12-23 23:40:07 +0300

    Update Startup.cs

--- ServerASPCORE/ServerASPCORE/Startup.cs
+++ ServerASPCORE/ServerASPCORE/Startup.cs
@@ -12,45 +12,36 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace ServerASPCORE
-{
-  public class Startup
   {
-    public Startup(IConfiguration configuration)
+  public class Startup
     {
+    public Startup(IConfiguration configuration)
+      {
       Configuration = configuration;
-    }
+      }
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
-    {
-
-      services.AddControllers();
-      services.AddSwaggerGen(c =>
       {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "ServerASPCORE", Version = "v1" });
-      });
+      services.AddControllers();
+      services.AddSwaggerGen(c =>{c.SwaggerDoc("v1", new OpenApiInfo { Title = "ServerASPCORE", Version = "v1" });});
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
       {
+      if (env.IsDevelopment())
+        {
         app.UseDeveloperExceptionPage();
         app.UseSwagger();
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ServerASPCORE v1"));
-      }
+        }
 
       app.UseRouting();
 
       app.UseAuthorization();
 
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
+      app.UseEndpoints(endpoints =>{endpoints.MapControllers();});
+      }
     }
   }
-}
