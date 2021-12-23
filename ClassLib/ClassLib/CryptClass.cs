c6e3e91c12e463967ffd58c923d3fc61521da43c
StepanGulyaev <91908161+StepanGulyaev@users.noreply.github.com>
2021-12-23 23:39:45 +0300

    Update CryptClass.cs

--- ClassLib/ClassLib/CryptClass.cs
+++ ClassLib/ClassLib/CryptClass.cs
@@ -6,42 +6,37 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassLib
-{
-    public class CryptClass
     {
-        public static string GetMD5(string input)
+    public class CryptClass
         {
-            // Use input string to calculate MD5 hash
-            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+        public static string GetMD5(string input)
             {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+                {
                 byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
                 byte[] hashBytes = md5.ComputeHash(inputBytes);
 
-                // Convert the byte array to hexadecimal string
                 StringBuilder sb = new StringBuilder();
                 for (int i = 0; i < hashBytes.Length; i++)
-                {
+                    {
                     sb.Append(hashBytes[i].ToString("X2"));
-                }
+                    }
                 return sb.ToString();
+                }
             }
-        }
         public static string GetSHA256(string input)
-        {
-            // Create a SHA256   
-            using (SHA256 sha256Hash = SHA256.Create())
             {
-                // ComputeHash - returns byte array  
+            using (SHA256 sha256Hash = SHA256.Create())
+                {
                 byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-
-                // Convert byte array to a string   
+ 
                 StringBuilder builder = new StringBuilder();
                 for (int i = 0; i < bytes.Length; i++)
-                {
+                    {
                     builder.Append(bytes[i].ToString("x2"));
-                }
+                    }
                 return builder.ToString();
+                }
             }
         }
     }
-}
