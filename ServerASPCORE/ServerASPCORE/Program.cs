de3a52150fbe165034ce430c85bc7a3c137d1417
StepanGulyaev <91908161+StepanGulyaev@users.noreply.github.com>
2021-12-23 23:40:01 +0300

    Update Program.cs

--- ServerASPCORE/ServerASPCORE/Program.cs
+++ ServerASPCORE/ServerASPCORE/Program.cs
@@ -9,37 +9,30 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace ServerASPCORE
-{
-  public class Program
   {
+  public class Program
+    {
     public static string LoginsFileName = "users.json";
     public static LoginMasClass storedLogins = new LoginMasClass(LoginsFileName);
     public static List<ClassLib.MessageClass> listOfMessages = new List<ClassLib.MessageClass>(); 
 
 
     public static void Main(string[] args)
-    {
-            listOfMessages.Clear();
-            ClassLib.MessageClass mes = new ClassLib.MessageClass();
-            mes.userName = "System";
-            mes.messageText = "Server is running...";
-            mes.timeStamp = DateTime.Now.ToString();
-            listOfMessages.Add(mes);
-            
-            
-            storedLogins.LoadLogins();
-            //storedLogins.RegUser("rusal", "1234");
+      {
+      listOfMessages.Clear();
+      ClassLib.MessageClass mes = new ClassLib.MessageClass();
+      mes.userName = "System";
+      mes.messageText = "Server is running...";
+      mes.timeStamp = DateTime.Now.ToString();
+      listOfMessages.Add(mes);
+           
+      storedLogins.LoadLogins();
 
-            CreateHostBuilder(args).Build().Run();
+      CreateHostBuilder(args).Build().Run();
 
-            storedLogins.SaveLogins();
+      storedLogins.SaveLogins();
+      }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>{webBuilder.UseStartup<Startup>();});
     }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
-  }
 }
