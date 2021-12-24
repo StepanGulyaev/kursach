e4888c12b9f92133f048a0cd7e9a0a01e541557a
StepanGulyaev <91908161+StepanGulyaev@users.noreply.github.com>
2021-12-24 21:51:48 +0300

    Update RegController.cs

--- ServerASPCORE/ServerASPCORE/Controllers/RegController.cs
+++ ServerASPCORE/ServerASPCORE/Controllers/RegController.cs
@@ -2,22 +2,18 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace ServerASPCORE.Controllers
-{
+    {
     [Route("api/[controller]")]
     [ApiController]
     public class RegController : ControllerBase
-    {
-        // POST api/<RegController>
+        {
         [HttpPost]
         public string Post(LoginClass reciveData)
-        {
-
+            {
             string token = Program.storedLogins.RegUser(reciveData.login, reciveData.password);
             Console.WriteLine(Program.storedLogins);
             return token;
+            }
         }
     }
-}
