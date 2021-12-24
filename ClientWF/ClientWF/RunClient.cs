b0d196653b069d4e99c56a0659fe8caa90ee34bd
StepanGulyaev <91908161+StepanGulyaev@users.noreply.github.com>
2021-12-24 21:51:58 +0300

    Create RunClient.cs

---
+++ ClientWF/ClientWF/RunClient.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientWF
+{
+  static class Program
+  {
+    /// <summary>
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Application.SetHighDpiMode(HighDpiMode.SystemAware);
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
+    }
+  }
+}
